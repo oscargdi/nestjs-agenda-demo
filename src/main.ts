4d58317d5cd765b199918fc648cfa97b3e50f45a
Oscar Diaz <oscargdi@hotmail.com>
2023-12-06 02:21:23 +0000

    add agendash dashboard

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Agenda } from 'agenda';
+// @ts-expect-error Agendash is not typed
+import * as Agendash from 'agendash';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const agenda = app.get<Agenda>(Agenda);
+
+  app.use('/agendash', Agendash(agenda));
+
   await app.listen(3000);
 }
 bootstrap();
